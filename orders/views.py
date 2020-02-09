913a95cda350cf36bb12f1117a31437b2d3b5275
radam9 <radam9@gmail.com>
2020-02-09 16:12:00 +0100

    changed images, modified base/menu html, auto populate html from DB

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from orders.models import Menu
 
 # Create your views here.
 def home(request):
@@ -6,5 +7,25 @@ def home(request):
 
 
 def menu(request):
-    return render(request, "orders/menu.html")
+    regular = Menu.objects.filter(category__item="Regular Pizza")
+    sicilian = Menu.objects.filter(category__item="Sicilian Pizza")
+    subs = Menu.objects.filter(category__item="Sub")
+    subl = subs[::2]
+    subr = subs[1::2]
+    pasta = Menu.objects.filter(category__item="Pasta")
+    salad = Menu.objects.filter(category__item="Salad")
+    dinner = Menu.objects.filter(category__item="Dinner Platter")
+    dinnerl = dinner[::2]
+    dinnerr = dinner[1::2]
+    menu = {
+        "regular": regular,
+        "sicilian": sicilian,
+        "subl": subl,
+        "subr": subr,
+        "pasta": pasta,
+        "salad": salad,
+        "dinnerl": dinnerl,
+        "dinnerr": dinnerr,
+    }
+    return render(request, "orders/menu.html", context=menu)
 
