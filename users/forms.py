0df8d02ba16e3f128e2cc3c08680f4d32851ce59
radam9 <radam9@gmail.com>
2020-02-10 12:47:59 +0100

    modified the login/register/logout html and css

--- users/forms.py
+++ users/forms.py
@@ -10,3 +10,8 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ["username", "email", "password1", "password2"]
 
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ["username", "password1", "password2"]:
+            self.fields[fieldname].help_text = None
