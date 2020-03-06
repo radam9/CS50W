b8b190e54ebb15d661ab4f51c1e21ca063287dc8
radam9 <radam9@gmail.com>
2020-03-06 21:09:41 +0100

    second commit

--- mydramas/views.py
+++ mydramas/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def dashboard(requests):
-    return HttpResponse("<h1>Hello, World!</h1>")
+def dashboard(request):
+    return render(request, "mydramas/dashboard.html")
+
+
+def dramalist(request):
+    return render(request, "mydramas/dramalist.html")
+
+
+def home(request):
+    return HttpResponse("<h1>Welcome to Home!</h1>")
