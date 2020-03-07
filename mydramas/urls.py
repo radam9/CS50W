ba8327228c59cafa75de39bbc6ca5f23097c8593
radam9 <radam9@gmail.com>
2020-03-07 22:05:39 +0100

    added REST API , updated dramalist page

--- mydramas/urls.py
+++ mydramas/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register("drama", views.DramaView)
+router.register("network", views.NetworkView)
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("api/", include(router.urls)),
     path("dashboard/", views.dashboard, name="dashboard"),
     path("dramalist/", views.dramalist, name="dramalist"),
 ]
--- mydramas/views.py
+++ mydramas/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Drama, Network
+from .serializers import DramaSerializer, NetworkSerializer
 
 
 def dashboard(request):
@@ -7,8 +10,19 @@ def dashboard(request):
 
 
 def dramalist(request):
-    return render(request, "mydramas/dramalist.html")
+    dramas = Drama.objects.all().order_by("title")
+    return render(request, "mydramas/dramalist.html", context={"dramas": dramas})
 
 
 def home(request):
     return HttpResponse("<h1>Welcome to Home!</h1>")
+
+
+class DramaView(viewsets.ModelViewSet):
+    queryset = Drama.objects.all()
+    serializer_class = DramaSerializer
+
+
+class NetworkView(viewsets.ModelViewSet):
+    queryset = Network.objects.all()
+    serializer_class = NetworkSerializer
